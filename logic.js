5b7c432a5de047ef084b1c8ccbec55854f5ed39c
Navnit Tiwari <42030960+navnittiwari@users.noreply.github.com>
2019-11-29 08:36:24 +0530

    initial uploads

---
+++ logic.js
@@ -0,0 +1,29 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;    //1-6 numbers
+
+var randomDiceImage="dice"+randomNumber1+".png";    //dice
+
+var randomImageSource="images/"+randomDiceImage;
+
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2=Math.floor(Math.random()*6)+1;
+
+var randomImageSource2="images/dice"+randomNumber2+".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//if player 1 wins
+if(randomNumber1>randomNumber2)
+{
+  document.querySelector("h1").innerHTML="👍 Player 1 wins!!";
+}
+
+else if(randomNumber1<randomNumber2)
+{
+  document.querySelector("h1").innerHTML="Player 2 wins 👍 !!";
+}
+
+else{
+  document.querySelector("h1").innerHTML="Opps!!!  its a tie, try again";
+}
